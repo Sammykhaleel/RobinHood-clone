c80eecdd6f8f2df197e5833de1225c233ace1e3c
sammy khaleel <sammykhaleel83@gmail.com>
2022-08-12 16:44:18 -0400

    css styling

---
+++ src/LineGraph.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Line} from 'react-chartjs-2'
+
+function LineGraph() {
+  return (
+    <div><Line /></div>
+  )
+}
+
+export default LineGraph
--- src/Newsfeed.js
+++ src/Newsfeed.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import LineGraph from "./LineGraph";
 import './Newsfeed.css'
 
 function Newsfeed() {
-  return (
-    <div className='newsfeed'>
-        <div className='newsfeed__container'>
+    return (
+        <div className='newsfeed'>
+            <div className='newsfeed__container'>
+                <div className='newsfeed__chartSection'>
+                    <div className='newsfeed__portfolio'>
+                        <h1>$25,500.65</h1>
+                        <p>35.78 (+0.08) Today</p>
+                    </div>
+                    <div className="newsfeed__chart">
+                        <LineGraph />
 
+                    </div>
+                </div>
+
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Newsfeed
