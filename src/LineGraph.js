a8479094fa0e87b7bd71b5dca02b022339de94a6
sammy khaleel <sammykhaleel83@gmail.com>
2022-08-14 10:38:29 -0400

    adding options and tootip to the lineGraph

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -17,21 +17,61 @@ import { Chart, registerables } from 'chart.js';
 Chart.register(...registerables);
 
 function LineGraph() {
-  const data = [{x:10,y:30},{x:30,y:50}]
+  const data = [
+    {
+      x:10,
+      y:20
+    },
+    {
+      x:15,
+      y:10
+    },
+    {
+      x:15,
+      y:100
+    }
+  ]
   return (
     <div><Line data ={ {
+      labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
         datasets: [{
-          label: 'My First Dataset',
+          // label: 'My First Dataset',
+          // type: "line",
+          // data: data,
+          // backgroundColor: 'rgba(255, 99, 132, 0.2)',  
+          // borderColor: 'rgb(255, 99, 132)',
+          // borderWidth: 1
+          
+          type: 'line',
+          backgroundColor: "black",
+          borderColor: "#5AC53B",
+          borderWidth: 2,
+          pointBorderColor: 'rgba(0, 0, 0, 0)',
+          pointBackgroundColor: 'rgba(0, 0, 0, 0)',
+          pointHoverBackgroundColor: '#5AC53B',
+          pointHoverBorderColor: '#000000',
+          pointHoverBorderWidth: 4,
+          pointHoverRadius: 6,
           data: data,
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)', 
-          ],
-          borderColor: [
-            'rgb(255, 99, 132)', 
-          ],
-          borderWidth: 1
         }]
-      }}/>
+      }}
+      options={{
+        plugins: {
+          tooltip: {
+              usePointStyle: true,
+              mode: "index",
+              intersect: false,
+          }
+        },
+        scales:{
+          yAxes:[{
+            ticks: {
+              display: false
+            }
+        }]
+        }
+      }}
+      />
     </div>
   )
 }
